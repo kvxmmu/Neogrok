cead428cad49d3ba002408a621f5de26d8d85f4a
Nero <resqnetm@gmail.com>
2022-12-01 02:13:54 +0300

    Add comments for the clap parser

--- client/lib/args.rs
+++ client/lib/args.rs
@@ -8,9 +8,13 @@ use {
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
+    /// Create TCP server
     Tcp {
+        /// Local address in format host:port, e.g.
+        /// localhost:25565
         address: String,
 
+        /// Port that will be bound on the server
         #[clap(long, short)]
         bind_port: Option<u16>,
     },
@@ -18,15 +22,19 @@ pub enum Command {
 
 #[derive(Debug, Parser)]
 pub struct Args {
+    /// Neogrok server address
     #[clap(long, short)]
     pub remote: String,
 
+    /// Number of worker threads, defaults to $(nproc)
     #[clap(long, short)]
     pub workers: Option<NonZeroUsize>,
 
+    /// Authorization magic
     #[clap(long, short)]
     pub password: Option<String>,
 
+    /// Action to perform
     #[clap(subcommand)]
     pub subcommand: Command,
 }
